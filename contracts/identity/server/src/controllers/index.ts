5666864eca666426a410839412b28f05cc748ccd
Steven Lam <stevenlam123@yahoo.com.hk>
2020-04-18 18:48:20 +0800

    clean project

--- contracts/identity/server/src/controllers/index.ts
+++
@@ -1,8 +0,0 @@
-export { AttributeExpressController } from './attribute.controller';
-export { OrganizationExpressController } from './organization.controller';
-export { PersonExpressController } from './person.controller';
-export { CredentialExpressController } from './credential.controller';
-export { DeveloperExpressController } from './developer.controller';
-export { ApplicationExpressController } from './application.controller';
-
-
